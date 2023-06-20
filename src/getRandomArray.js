6d5ebf993fe045767868c830b1f501b0a4747a14
dotunpeters <oyelakindotun@gmail.com>
2023-06-20 02:34:17 +0100

    getRandomArray

---
+++ src/getRandomArray.js
@@ -0,0 +1,12 @@
+import * as logs from "./data.json";
+
+function randomizeArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+const randomizedArray = randomizeArray(logs);
+console.log(randomizedArray);
